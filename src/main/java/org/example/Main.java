3e81e6d80df480b66cc2ec63b40fafc392e2334c
Jaime Lorenzo S. Palacio <itsjustmejaime77@gmail.com>
2026-02-20 18:48:12 +0800

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Terminal Adventure");
+        System.out.println("-----------------------------");
+
+        System.out.println("Greetings Adventurer!");
+        System.out.print("What is your name?: ");
+        String userName = input.nextLine();
+        System.out.println("-----------------------------");
+
+        System.out.println("Welcome " + userName + "!");
+        System.out.println("Press enter to continue");
+        input.nextLine();
+
+
+        System.out.println("Hello");
+
+    }
+}
