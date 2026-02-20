d072c9d7d35fe28e35e80bf777381675d3fef357
Jaime Lorenzo S. Palacio <itsjustmejaime77@gmail.com>
2026-02-20 19:06:32 +0800

    third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,6 +19,30 @@ public class Main {
 
         System.out.println("-----------------------------");
         System.out.println("Hello");
+        input.nextLine();
+
+        System.out.println("My name is Pixel");
+        input.nextLine();
+
+        System.out.println("Will you explore this world with me?");
+        input.nextLine();
+
+        while(true) {
+            System.out.println("Your reply (yes/no): ");
+            String reply = input.nextLine();
+
+            if (reply.equalsIgnoreCase("yes") || reply.equalsIgnoreCase("y")) {
+                System.out.println("You wake up in the middle of a field with ragged clothes and a sword.");
+                System.out.println("You are almost alone, if not for the voice that is narrating.");
+                break;
+            } else if (reply.equalsIgnoreCase("no") || reply.equalsIgnoreCase("n")) {
+                System.out.println(reply + " is not a valid option my friend");
+                continue;
+            } else {
+                System.out.println("You find yourself breathless, as the adventure has ended before it has even begun");
+                continue;
+            }
+        }
 
     }
 }
